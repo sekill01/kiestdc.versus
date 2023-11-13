9d6f433900d50d5487a735bf2dd0fb9dd4ee28c9
sekill01 <80636880+sekill01@users.noreply.github.com>
2023-11-13 18:56:17 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -46,8 +46,7 @@ Oy ver (YAKINDA)</a>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
           <div className="col-span-4">
             <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Kiest Bot" }} />
-            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "Botumuz  ile eğlenmeye hazırmısınmısın ? botumuzu sunucunuza ekleyerek eğlenebilirsiniz.
-bot hakkındaki düşüncelerinizi discorddan yazabilirsiniz" }}/>
+            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
             <a href="https://bit.ly/47fJeDt" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
               <i className="fab fa-discord mr-2" />Botu Ekle
             </a>
